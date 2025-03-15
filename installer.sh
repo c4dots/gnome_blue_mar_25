e6cbbe0f954ed64d051f64852a62b6455031bfc1
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-03-15 17:49:01 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Go into cache folder
-mkdir ~/.dotcache/
+mkdir ~/.dotcache/ &> /dev/null
 cd ~/.dotcache/
 
 # Clone repo
-git clone https://github.com/c4dots/gnome_blue_mar_25/
+git clone https://github.com/c4dots/gnome_blue_mar_25/ &> /dev/null
 cd gnome_blue_mar_25
 
 # Run setup
