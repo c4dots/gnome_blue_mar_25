e3856d753fd21dbf3c605cbdc5f6ec4195b04d67
c4vxl <141231805+c4vxl@users.noreply.github.com>
2025-04-17 11:37:18 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -8,6 +8,7 @@ sudo chmod 777 -R ~/.cache/.dotcache/
 # Clone repo
 sudo -S git clone https://github.com/c4dots/gnome_blue_mar_25/ &> /dev/null
 cd gnome_blue_mar_25
+sudo chmod 777 -R .
 
 # Run setup
 bash setup.sh "$@"
